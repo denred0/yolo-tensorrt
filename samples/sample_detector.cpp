43145b86394619933563cfb3d26d4391d41335a5
Denis Potapov <potapovdenisdmit@gmail.com>
2022-02-16 11:01:27 +0500

    Update sample_detector.cpp

--- samples/sample_detector.cpp
+++ samples/sample_detector.cpp
@@ -9,24 +9,29 @@
 #include <experimental/filesystem>
 #include <filesystem>
 
+#ifdef __linux__
+	std::string separator = "/";
+#elif _WIN32
+	std::string separator = "\\";
+#endif
 
-std::vector<std::string> get_filenames( std::experimental::filesystem::path path )
+std::vector<std::string> get_filenames(std::experimental::filesystem::path path)
 {
-    namespace stdfs = std::experimental::filesystem ;
+	namespace stdfs = std::experimental::filesystem;
 
-    std::vector<std::string> filenames ;
+	std::vector<std::string> filenames;
 
-    // http://en.cppreference.com/w/cpp/experimental/fs/directory_iterator
-    const stdfs::directory_iterator end{} ;
+	// http://en.cppreference.com/w/cpp/experimental/fs/directory_iterator
+	const stdfs::directory_iterator end{};
 
-    for( stdfs::directory_iterator iter{path} ; iter != end ; ++iter )
-    {
-        // http://en.cppreference.com/w/cpp/experimental/fs/is_regular_file
-        if( stdfs::is_regular_file(*iter) ) // comment out if all names (names of directories tc.) are required
-            filenames.push_back( iter->path().string() ) ;
-    }
+	for (stdfs::directory_iterator iter{path}; iter != end; ++iter)
+	{
+		// http://en.cppreference.com/w/cpp/experimental/fs/is_regular_file
+		if (stdfs::is_regular_file(*iter)) // comment out if all names (names of directories tc.) are required
+			filenames.push_back(iter->path().string());
+	}
 
-    return filenames ;
+	return filenames;
 }
 
 int main()
@@ -61,22 +66,12 @@ int main()
 
 	std::unique_ptr<tensor_rt::Detector> detector(new tensor_rt::Detector());
 	detector->init(config_v5);
-
 	std::vector<tensor_rt::BatchResult> batch_res;
-
-
-
-	#ifdef __linux__
-    	std::string separator = "/";
-    #elif _WIN32
-        std::string separator = "\\";
-    #endif
-
 
 	std::string root_dir = "../samples/configs/yolov5/attributes/test_images";
 	std::string output_dir = "../samples/configs/yolov5/attributes/test_images_result";
 	std::vector<std::string> filenames = get_filenames(root_dir);
-	int n_batch = 1;//config_v4.batch_size;
+	int n_batch = 1; // config_v4.batch_size;
 
 	Timer timer;
 	cv::Mat frame;
@@ -86,11 +81,10 @@ int main()
 	std::vector<std::string> bboxes_result;
 	std::string result = "";
 
-
+	using std::chrono::duration;
+	using std::chrono::duration_cast;
 	using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::milliseconds;
+	using std::chrono::milliseconds;
 
 	double duration_time = 0;
 	int inference_count = 0;
@@ -99,22 +93,23 @@ int main()
 	while (index < filenames.size())
 	{
 
-		//prepare batch data
+		// prepare batch data
 		batch_img.clear();
 
-		for(int bi=0; bi<n_batch; ++bi){
-            frame = cv::imread(filenames[index]);
-            index++;
+		for (int bi = 0; bi < n_batch; ++bi)
+		{
+			frame = cv::imread(filenames[index]);
+			index++;
 
-            if(!frame.data)
-                break;
+			if (!frame.data)
+				break;
 
-            batch_img.push_back(frame);
-        }
+			batch_img.push_back(frame);
+		}
 
 		bboxes_result.clear();
 
-		//detect
+		// detect
 		auto t1 = high_resolution_clock::now();
 		timer.reset();
 		detector->detect(batch_img, batch_res);
@@ -126,34 +121,32 @@ int main()
 		std::cout << "Inference time: " + std::to_string(ms_double.count() / 1000) + " sec \n";
 		inference_count = inference_count + 1;
 
-		//disp
-		for (int i=0;i<batch_img.size();++i)
+		// disp
+		for (int i = 0; i < batch_img.size(); ++i)
 		{
 			for (const auto &r : batch_res[i])
 			{
-				std::cout <<"batch "<<i<< " id:" << r.id << " prob:" << r.prob << " rect:" << r.rect << std::endl;
+				std::cout << "batch " << i << " id:" << r.id << " prob:" << r.prob << " rect:" << r.rect << std::endl;
 				cv::rectangle(batch_img[i], r.rect, cv::Scalar(255, 0, 0), 2);
 				std::stringstream stream;
 				stream << std::fixed << std::setprecision(2) << "id:" << r.id << "  score:" << r.prob;
 				cv::putText(batch_img[i], stream.str(), cv::Point(r.rect.x, r.rect.y - 5), 0, 0.5, cv::Scalar(0, 0, 255), 2);
 
-
 				result = std::to_string(r.id) + " " +
-                    std::to_string(r.prob) + " " +
-                    std::to_string(r.rect.x) + " " +
-                    std::to_string(r.rect.y) + " " +
-                    std::to_string(r.rect.x + r.rect.width) + " " +
-                    std::to_string(r.rect.y + r.rect.height);
-                bboxes_result.push_back(result);
-
+						 std::to_string(r.prob) + " " +
+						 std::to_string(r.rect.x) + " " +
+						 std::to_string(r.rect.y) + " " +
+						 std::to_string(r.rect.x + r.rect.width) + " " +
+						 std::to_string(r.rect.y + r.rect.height);
+				bboxes_result.push_back(result);
 			}
 
 			std::string filepath = filenames[image_counter];
-			std::experimental::filesystem::path my_path {filepath};
-
+			std::experimental::filesystem::path my_path{filepath};
 
 			std::ofstream outFile(output_dir + separator + my_path.stem().string() + ".txt");
-        	for (const auto &e : bboxes_result) outFile << e << "\n";
+			for (const auto &e : bboxes_result)
+				outFile << e << "\n";
 
 			cv::imwrite(output_dir + separator + my_path.filename().string(), batch_img[i]);
 			image_counter++;
@@ -167,5 +160,4 @@ int main()
 	std::cout << "FPS Inference only: " + std::to_string(filenames.size() / duration_time) + '\n';
 	std::cout << "Duration: " + std::to_string(ms_double.count()) + '\n';
 	std::cout << "Avg Inference time: " + std::to_string(duration_time / (filenames.size())) + '\n';
-
 }
